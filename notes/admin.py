9a8acba6dd32468b04270e5cf5ccc78ca0cff274
Zero-Ace5 <nitrovictory@outlook.com>
2025-12-18 22:45:55 +0530

    Added Admin

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Note
 
 # Register your models here.
+# admin.site.register(Note)
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ("id", "text", "created")
+    ordering = ("-text",)
